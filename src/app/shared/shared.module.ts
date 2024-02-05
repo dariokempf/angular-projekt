4bd174a0811260231212498feed90abc7b1e554c
dariokempf <dario.kempf@edu.vs.ch>
2024-02-05 15:37:52 +0100

    alert function

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleAComponent } from './example-a/example-a.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlertModule
   ],
   declarations: [ExampleAComponent],
   exports:  [ExampleAComponent],
